1e61c33ab93f13b628525672eb9f16b1111bd3b7
Urs Peter <peter.urs@gmail.com>
2011-05-02 18:00:58 +0200

    rest interface for Create/Read/Update of XKESessions

---
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -0,0 +1,58 @@
+package bootstrap.liftweb
+
+import _root_.net.liftweb.util._
+import _root_.net.liftweb.common._
+import _root_.net.liftweb.http._
+import _root_.net.liftweb.http.provider._
+import _root_.net.liftweb.sitemap._
+import _root_.net.liftweb.sitemap.Loc._
+import Helpers._
+import _root_.net.liftweb.mapper.{DB, ConnectionManager, Schemifier, DefaultConnectionIdentifier, StandardDBVendor}
+import _root_.java.sql.{Connection, DriverManager}
+import _root_.com.xebia.model._
+import _root_.com.xebia.rest._
+
+
+/**
+ * A class that's instantiated early and run.  It allows the application
+ * to modify lift's environment
+ */
+class Boot {
+  def boot {
+
+    // where to search snippet
+    LiftRules.addToPackages("com.xebia")
+
+    // Build SiteMap
+    def sitemap() = SiteMap(
+      Menu("Home") / "index" >> User.AddUserMenusAfter, // Simple menu form
+      // Menu with special Link
+      Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
+	       "Static Content")))
+
+    LiftRules.setSiteMapFunc(() => User.sitemapMutator(sitemap()))
+
+    LiftRules.dispatch.append(XKENGDispatchAPI)
+    /*
+     * Show the spinny image when an Ajax call starts
+     */
+    LiftRules.ajaxStart =
+      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+
+    /*
+     * Make the spinny image go away when it ends
+     */
+    LiftRules.ajaxEnd =
+      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+
+    LiftRules.early.append(makeUtf8)
+
+  }
+
+  /**
+   * Force the request to be UTF-8
+   */
+  private def makeUtf8(req: HTTPRequest) {
+    req.setCharacterEncoding("UTF-8")
+  }
+}
