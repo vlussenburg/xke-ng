3a6743893f87bfec7656223a2e3633c5beb2e5d7
lemval <mvanleeuwen@xebia.com>
2011-10-29 11:21:42 +0200

    Increased timeout (is in millis?)

--- xke-ng-web/src/test/scala/com/xebia/xkeng/db/MongoTestConnection.scala
+++ xke-ng-web/src/test/scala/com/xebia/xkeng/db/MongoTestConnection.scala
@@ -9,7 +9,7 @@ import net.liftweb.mongodb.MongoDB
 trait MongoTestConnection {
   val srvr = new ServerAddress("127.0.0.1", 27017)
   val mo = new MongoOptions
-  mo.socketTimeout = 10
+  mo.socketTimeout = 100
 
   def init() =  MongoDB.defineDb(DefaultMongoIdentifier, new Mongo(srvr, mo), "xkeng_test")
 }
