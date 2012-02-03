b548b1ad92da3c08a1f631cf5512eec026bc5aa5
Urs <urs_peter@gmx.ch>
2012-02-03 11:10:03 +0100

    add findNextConference to Conference repository

--- xke-ng-web/src/test/scala/com/xebia/xkeng/dao/ConferenceRepositoryTest.scala
+++ xke-ng-web/src/test/scala/com/xebia/xkeng/dao/ConferenceRepositoryTest.scala
@@ -22,13 +22,14 @@ class ConferenceRepositoryTest extends FlatSpec with ShouldMatchers with BeforeA
   val xke2011_06_17 = fmt.parseDateTime("2011-06-17T16:00:00.000Z")
   val xke2011_05_03 = fmt.parseDateTime("2011-05-03T16:00:00.000Z")
   val xke2010_05_01 = fmt.parseDateTime("2010-05-01T16:00:00.000Z")
-
+  val xkeFuture1 = new DateTime().plusWeeks(1);
+  val xkeFuture2 = new DateTime().plusWeeks(3);
   var conferences: List[Conference] = Nil
 
   override def beforeEach() {
     init()
     Conference.drop
-    val dates = xke2010_05_01 :: xke2011_05_03 :: xke2011_06_03 :: xke2011_06_17 :: Nil
+    val dates = xke2010_05_01 :: xke2011_05_03 :: xke2011_06_03 :: xke2011_06_17 :: xkeFuture1 :: xkeFuture2 :: Nil
     conferences = dates.map(createTestConference(_))
   }
 
@@ -44,6 +45,19 @@ class ConferenceRepositoryTest extends FlatSpec with ShouldMatchers with BeforeA
     val confs = conferenceRepository.findConferences(xke2011_06_03.getYear)
     confs.isEmpty should not be true
     confs.size should equal(3)
+  }
+
+  it should "find next conference " in {
+    var conf = conferenceRepository.findNextConference(1)
+    conf should not be None
+    val Some(c1) = conf
+    c1.begin should be(xkeFuture1)
+    conf = conferenceRepository.findNextConference(2)
+    conf should not be None
+    val Some(c2) = conf
+    c2.begin should be(xkeFuture2)
+    conf = conferenceRepository.findNextConference(3)
+    conf should be(None)
   }
 
   it should "find conference by year and month" in {
